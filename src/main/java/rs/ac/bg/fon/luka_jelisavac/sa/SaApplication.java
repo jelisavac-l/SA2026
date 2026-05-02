7db7d05599398c6148669e8c4ad13e5d155ba3f0
jelisavac-l <jelisavacluka63@gmail.com>
2026-05-02 20:29:31 +0200

    init

---
+++ src/main/java/rs/ac/bg/fon/luka_jelisavac/sa/SaApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.luka_jelisavac.sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/bg/fon/luka_jelisavac/sa/SaApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.luka_jelisavac.sa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
